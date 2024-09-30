bc02022d57c0f20f837b7ff41257c104bce089ee
Ceren Canbaz <ceren_canbaz@hotmail.com>
2024-09-30 17:02:44 +0300

    first commit

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Ceren Canbaz on 30.09.2024.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock").clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/).overlay{Circle().stroke(.white,lineWidth: 4)}.shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
