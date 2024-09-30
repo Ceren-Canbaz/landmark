9c70ce8d75339eb7a9df6a57800cd23282d3aa26
Ceren Canbaz <c.canbaz@digitastic.app>
2024-09-30 15:57:29 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ceren Canbaz on 30.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
