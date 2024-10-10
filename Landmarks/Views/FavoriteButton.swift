eacd9f58c951c4d1afab751e4996814e82bc54ef
Ceren Canbaz <ceren_canbaz@hotmail.com>
2024-10-10 11:03:46 +0300

    doc: documentation added for FavoriteButton

--- Landmarks/Views/FavoriteButton.swift
+++ Landmarks/Views/FavoriteButton.swift
@@ -1,20 +1,25 @@
 
 
 import SwiftUI
-
+// A button that toggles the favorite state, represent by a star icon
 struct FavoriteButton: View {
-    @Binding var isSet:Bool
+    @Binding var isSet:Bool // Binding to track the favorite state
     var body: some View {
         Button{
-            isSet.toggle()
+            isSet.toggle() // Toggle the favorite state when the button is pressed
         } label: {
-            Label("Toggle Favorite",systemImage: isSet ? "star.fill" : "star").labelStyle(.iconOnly).foregroundColor(isSet ? .yellow : .gray
+            // Display a star icon that changes based on the favorite state
+            Label("Toggle Favorite",systemImage: isSet ? "star.fill" : "star").labelStyle(.iconOnly).foregroundColor(isSet ? .yellow : .gray // Change color based on favorite state
             )
         }
         
     }
 }
-
+//Preview with the favorite state
 #Preview {
-    FavoriteButton(isSet:.constant(true))
+    Group{
+        FavoriteButton(isSet:.constant(true))
+        FavoriteButton(isSet:.constant(false))
+    }
+    
 }
